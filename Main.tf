d3a39327d49c39fbcf00dfd500031e98b3557845
deepakkuma10038 <deepakkumar10038@gmail.com>
2025-08-28 19:45:17 +0530

    pipeline new

--- Main.tf
+++ Main.tf
@@ -1,12 +1,12 @@
 
 
 resource "azurerm_resource_group" "rg" {
-  name     = "deep56"
+  name     = "deep56rg"
   location = "centralus"
 }
 resource "azurerm_storage_account" "stg" {
   name                     = "deepstg58"
-  resource_group_name      = "deep56"
+  resource_group_name      = "deep56rg"
   location                 = "centralus"
   account_tier             = "Standard"
   account_replication_type = "LRS"
