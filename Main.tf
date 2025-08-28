81936b3e6c50fff7c661b91f133ee3ce55f4ee8e
deepakkuma10038 <deepakkumar10038@gmail.com>
2025-08-28 20:03:15 +0530

    mew1

--- Main.tf
+++ Main.tf
@@ -1,13 +1,13 @@
 
 
 resource "azurerm_resource_group" "rg" {
-  name     = "deep56rg"
+  name     = "deep-rg"
   location = "centralus"
 }
 resource "azurerm_storage_account" "stg" {
   depends_on = [ azurerm_resource_group.rg ]
-  name                     = "deepstg58"
-  resource_group_name      = "deep56rg"
+  name                     = "deep1stg"
+  resource_group_name      = "deep-rg"
   location                 = "centralus"
   account_tier             = "Standard"
   account_replication_type = "LRS"
